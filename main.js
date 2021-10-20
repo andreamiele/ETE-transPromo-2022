810ae156cf1e10e68494747931c92c16011171da
Andrea MIELE <andrea.miele.pro@gmail.com>
2021-10-20 19:40:33 +0200

    Début du jeu 1

---
+++ main.js
@@ -0,0 +1,93 @@
+const divResultat = document.querySelector("#resultat");
+
+var tabJeu = [
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,1,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+]
+
+
+
+
+var tabJeu2 = [
+    Array.apply(null, Array(6)).map(function() { return Math.floor(Math.random() * 100 % 100); }),
+    Array.apply(null, Array(6)).map(function() { return Math.floor(Math.random() * 100 % 100); }),
+    Array.apply(null, Array(6)).map(function() { return Math.floor(Math.random() * 100 % 100); }),
+    Array.apply(null, Array(6)).map(function() { return Math.floor(Math.random() * 100 % 100); }),
+    Array.apply(null, Array(6)).map(function() { return Math.floor(Math.random() * 100 % 100); }),
+    Array.apply(null, Array(6)).map(function() { return Math.floor(Math.random() * 100 % 100); }),
+];
+
+var sourireColonne = entierAleatoire(0,5);
+var sourireLigne = entierAleatoire(0,5);
+var sourireValeur = tabJeu2[sourireLigne][sourireColonne];
+
+
+
+afficherTableau();
+
+function afficherTableau(){
+    var txt ="";
+
+    for(var i=0;i <tabJeu.length;i++)
+    {
+        txt +="<div>";
+        for(var j=0;j<tabJeu[i].length;j++)
+        {
+            if((i==sourireColonne)&&(j==sourireLigne)) {
+                txt += "<button id ='bonbouton' class='btn btn-primary m-2' style='width:100px;height:100px'> oui </button>";
+            }
+            else
+            {
+                txt +="<img src='img/logo.png' style='width:100px;height:100px' class='m-2'>";
+            }
+        }
+    txt +="</div>";
+    }
+
+    divResultat.innerHTML = txt;
+}
+
+function getImage(valeur){
+    switch(valeur){
+        case 1 :
+            break
+    }
+}
+
+
+function entierAleatoire(min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Compte à rebours en arrivant sur le page
+
+var i=5;
+const texte = document.querySelector('h2');
+
+intervalID = setInterval(function() {
+
+    //alert(coucou[i].innerHTML);
+    if (i>=0) {
+
+        texte.innerText=i;
+    i = i - 1;}
+    else {
+        temps1 = new Date().getTime()
+        texte.innerText="A toi de jouer !";
+    }
+
+
+}, 1000);
+
+var temps1 = new Date();
+var temps2 = new Date();
+
+function getTimer(){
+    temps2 = new Date().getTime();
+    texte.innerText=temps2-temps1+"s";
+}
