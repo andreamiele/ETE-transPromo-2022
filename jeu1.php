206515f0f69a628e6637c4718f0996f19be1a2ae
Andrea MIELE <andrea.miele.pro@gmail.com>
2021-10-25 14:51:45 +0200

    Commentaires

--- jeu1.php
+++ jeu1.php
@@ -5,10 +5,10 @@ $image_Souri = glob("/img/Jeu1/Sourires/{*.jpg, *.JPG, *.JPEG, *.png, *.PNG}", G
 
 
 // shuffle($all_images); // Pour randomiser, décommenter la ligne
-// shuffle($image_Souri);
+// shuffle($image_Souri); // Pareil
 
-$imagesN = array();
-$imagesS = array();
+$imagesN = array(); // Images sans sourire
+$imagesS = array(); // Image sourire
 
 foreach ($all_images as $index => $image) {
     if ($index == 35) break;  // On ne prends que 35 images qui ne sourient pas
--- styleJeu1.php
+++ styleJeu1.php
@@ -3,3 +3,6 @@
         background-image: url("img/Jeu1/Sourires/' jArray2 .png");
     }
 </style>
+
+<?php
+// Faudrait pouvoir concatener des listes en css
