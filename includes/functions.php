7d91e6454224c864d40ff9c8ea4b5171cbba9f18
Arnaud BASCOP <arnaudbscp@outlook.fr>
2021-11-15 17:14:54 +0100

    Inscription

--- includes/functions.php
+++ includes/functions.php
@@ -18,26 +18,8 @@ function redirect($url) {
 function isUserConnected() {
     return isset($_SESSION['id']);
 }
-/*
-function inscription($mdp, $email, $nom, $prenom, $adre, $code, $tel, $prom, $valide) {
-    // Inscription du compte
-    $reqInscriptionCompte = getDb()->prepare('INSERT INTO compte(mdp, email, role) VALUES(?, ?, ?)');
-    $reqInscriptionCompte->execute(array($mdp, $email, 1));
 
-    // Inscription de l'élève
-    $reqInscriptionEleve = getDb()->prepare('INSERT INTO eleve(nom_eleve, prenom_eleve, adresse_eleve, 
-                  adresse_visible_eleve, code_postal_eleve, code_postal_visible_eleve, telephone_eleve, 
-                  telephone_visible_eleve, url_photo, genre_eleve, promotion_eleve, id_compte, valide, description_eleve)  
-                   VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
-
-    // On recherche l'ID du compte pour le lier à l'élève
-    $reqGetId = getDb()->prepare('SELECT * FROM compte WHERE email=?');
-    $reqGetId->execute(array($email));
-    if ($reqGetId->rowCount() == 1) {
-        $id = -1;
-        while ($res = $reqGetId->fetch()) {
-            $id = $res['ID_COMPTE'];
-        }
-        $reqInscriptionEleve->execute(array($nom, $prenom, $adre, 0, $code, 0, $tel, 0, 'img/profil.png', '', $prom, $id , $valide, ""));
-    }
-}*/
+function inscription($email, $mdp, $prenom, $nom, $avatar, $couleur) {
+    $reqInscriptionCompte = getDb()->prepare('INSERT INTO users(email, password, prenom, nom, avatar, couleur, derniereConnexion, avancementJeu, score, DonneesJeuMatrice, DonneesJeuCroises) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
+    $reqInscriptionCompte->execute(array($email, $mdp, $prenom, $nom, $avatar, $couleur, date('Y-m-d H:i:s'), NULL, 0, NULL, NULL));
+}
--- index.php
+++ index.php
@@ -10,6 +10,11 @@ if (isset($_GET['error'])) { ?>
     <div class="alert alert-danger">
         Mauvais identifiants...
     </div>
+<?php }
+if (isset($_GET['inscription'])) { ?>
+    <div class="alert alert-success">
+        Félicitations ! Merci de vous connecter.
+    </div>
 <?php } ?>
 
 <!DOCTYPE html>
@@ -18,7 +23,7 @@ if (isset($_GET['error'])) { ?>
 <head>
     <meta charset="UTF-8"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <title>ETƎ</title>
+    <title>ETƎ - Connexion</title>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/index.css"/>
@@ -41,7 +46,7 @@ if (isset($_GET['error'])) { ?>
             </div>
             <button type="submit" class="btn btn-primary">CONNEXION</button>
             <hr/>
-            <h5>Pas encore de compte ? <a href="">S'inscrire</a></h5>
+            <h5>Pas encore de compte ? <a href="inscription.php">S'inscrire</a></h5>
         </form>
     </div>
 </section>
